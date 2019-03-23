0ba4419b824dbc67e5f09e9afdf6671a9fb0dac3
japedersen <jonna.pedersen@gmail.com>
2019-03-23 15:58:42 -0400

    prefabs without colliders are ready
    you can see all the furniture laid out in the furniture scene and i used the prefabs scene to just organize and make the prefabs

---
+++ Assets/Scripts/Instructions.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instructions : MonoBehaviour
+{
+
+    [Header("HOW TO USE THIS OBJECT:")]
+
+    [Header("1. Assemble it how you want")]
+
+    [Header("2. Make an empty game object with the same name")]
+
+    [Header("3. Make this object the child")]
+
+    [Header("4. Set the parent LAYER to 'Real World Objects' ")]
+
+    [Header("5. Set the parent TAG to 'Stackable', 'Surface', or 'Both' ")]
+
+    [Header("STACKABLE = can be placed on something")]
+
+    [Header("SURFACE = something can be placed on this")]
+
+    [Header("BOTH = both stackable and surface")]
+
+    [Header("6. Add 'ROTATE' script to parent object")]
+
+    [Header("7. Add Rigidbody without gravity to parent object")]
+
+    [Header("8. Add colliders to parent object")]
+
+    [Header("9. .....")]
+
+    [Header("10. ...profit????")]
+
+    public bool goodJob;
+
+}
