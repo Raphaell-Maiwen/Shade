3f086c3befefa373c5ae7e40596d6f48309607c1
japedersen <jonna.pedersen@gmail.com>
2019-04-14 16:48:12 -0400

    shade idle/run/jump + animation controller in Animation Gym scene
    jump is real janky and controller is unfinished

---
+++ Assets/_Scripts/ShadeAnimated.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadeAnimated : MonoBehaviour
+{
+    public float speed;
+    public float jumpVelocity;
+
+    Rigidbody rb;
+
+    public Animator anim;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetBool("isRunning", false);
+    }
+
+    void Update()
+    {
+        ManageMovement();
+
+        
+    }
+
+    public void ManageMovement()
+    {
+        if (Input.GetAxis("Horizontal") !=0)
+        {
+            Run();
+        }
+
+        if (Input.GetAxis("Horizontal") == 0)
+        {
+            anim.SetBool("isRunning", false);
+        }
+
+        print(rb.velocity.y);
+        if (Input.GetKeyDown(KeyCode.Space) && rb.velocity.y < 0.0001f && rb.velocity.y > -0.0001f)
+        {
+            Jump();
+        }
+    }
+
+    public void Run()
+    {
+        transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime);
+        anim.SetBool("isRunning", true);
+
+    }
+
+    public void Jump()
+    {
+        rb.velocity = jumpVelocity * Vector3.up;
+        anim.SetTrigger("isJumping");
+    }
+}
