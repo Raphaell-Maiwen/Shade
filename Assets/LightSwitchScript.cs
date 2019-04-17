743b589ae1dfbd1a7efc023bd5e7fc04771db381
Kiddy9s9Park <47836863+Kiddy9s9Park@users.noreply.github.com>
2019-04-16 23:02:56 -0400

    have to COMMIT

---
+++ Assets/LightSwitchScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitchScript : MonoBehaviour
+{
+
+    public GameObject light01;
+    public GameObject light02;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        light01.gameObject.SetActive(false);
+        light02.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            light01.gameObject.SetActive(true);
+            light02.gameObject.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            light01.gameObject.SetActive(false);
+            light02.gameObject.SetActive(true);
+        }
+    }
+}
