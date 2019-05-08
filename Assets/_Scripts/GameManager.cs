a73fe0eaf6b8f8eddeed448267890e713839bcb3
Rafiki <raphaelle.mp@gmail.com>
2019-05-08 18:32:27 -0400

    Fixed the merge??

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
     public Avatar avatarScript;
@@ -17,6 +18,8 @@ public class GameManager : MonoBehaviour {
     public AudioSource elliotTheme;
 
     GameObject player;
+
+    public string nextLevel;
 
 	void Awake () {
         avatarScript.enabled = true;
@@ -112,5 +115,11 @@ public class GameManager : MonoBehaviour {
         }
 
         return objectsToProject.ToArray();
+    }
+
+    public void NextLevel() {
+        shadowCharacterScript.enabled = false;
+        avatarScript.enabled = false;
+        SceneManager.LoadScene(nextLevel);
     }
 }
