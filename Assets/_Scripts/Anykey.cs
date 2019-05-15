2aab29e13598326059a1749cdb2b620a49258e4d
Kiddy9s9Park <47836863+Kiddy9s9Park@users.noreply.github.com>
2019-05-15 19:45:23 -0400

    Modifications for build

---
+++ Assets/_Scripts/Anykey.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Anykey : MonoBehaviour
+{
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+            SceneManager.LoadScene("Main_Menus");
+            
+    }
+}
