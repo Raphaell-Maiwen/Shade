8a457229880e745a3d0baf376925f1ef7ce26122
Rafiki <raphaelle.mp@gmail.com>
2019-05-15 03:51:30 -0400

    Main menu working - only missing the credits scene

---
+++ Assets/_Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button newGame;
+    public Button credits;
+    public Button quit;
+
+    void Awake()
+    {
+        newGame.onClick.AddListener(StartNewGame);
+        credits.onClick.AddListener(ShowCredits);
+        quit.onClick.AddListener(QuitGame);
+    }
+
+    void StartNewGame() {
+        SceneManager.LoadScene("Tutorial_Level_01");
+    }
+
+    void ShowCredits() {
+        //SceneManager.LoadScene("Credits");
+    }
+
+    void QuitGame() {
+        print("Quit Game");
+        Application.Quit();
+    }
+}
